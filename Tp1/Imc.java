890f5b0f979c956ef1cd9b58990feae6e9cb2d28
LenScarlet666 <106494253+LenScarlet666@users.noreply.github.com>
2023-10-12 13:00:54 -0300

    Update Imc.java

--- Tp1/Imc.java
+++ Tp1/Imc.java
@@ -1,36 +1,28 @@
-public class Imc{
-    public static void main (String[]args){
+public class Imc {
+    public static void main(String[] args) {
         System.out.println("Calclulo de IMC");
-      
-        System.out.print("Ingrese su altura:");
-        double altura=new java.util.Scanner(System.in).nextDouble();
-        System.out.print("Ingrese su peso:");        
-        double peso=new java.util.Scanner(System.in).nextDouble();
-        double Imc=(peso/(altura*2));
-       
-        if(Imc<=15){
-        System.out.print("Su imc es:" + Imc + "Delgadez muy severa");
-        
-        } 
-         else if(Imc>15 && Imc<15.9 ){
-            System.out.println("Su imc es de:"+ Imc + " Delgadez severa");
-        }
-        else if(Imc>=16 && Imc<18.4 ){
-            System.out.println("Su imc es: "+ Imc + "Delgadez");
-        }
-        else if ( Imc>=18.5 && Imc<=24.9)
-            System.out.println("Su IMC es:" + Imc + " Normal");
-       
-        else if (Imc>=25 && Imc<=29.9 ){
-            System.out.println("Su imc es de:"+ Imc + " Sobrepeso");
-        }
-        else if(Imc>=30 && Imc<=39.9 ){
-            System.out.println("Su imc es: "+ Imc + "Sobrepeso");
-        }
-        else{
-            System.out.println("Su IMC es:" + Imc + " Obesidad morbida");
-        }
-        }
-        }
 
-    
+        System.out.print("Ingrese su altura en cm:");
+        double altura = new java.util.Scanner(System.in).nextDouble();
+        System.out.print("Ingrese su peso en Kg:");
+        double peso = new java.util.Scanner(System.in).nextDouble();
+        double imc = (peso / (altura / 100 * altura / 100));
+
+        if (imc <= 15)                          System.out.println("Su imc es de:" + imc + "Delgadez muy severa");
+
+        if (imc > 15 && Imc < 15.9)             System.out.println("Su IMC es de: " + imc + "Delgadez severa");
+
+        if (imc >= 16 && Imc < 18.4)            System.out.println("Su IMC es de: " + imc + "Delgadez");
+
+        if (imc >= 18.5 && Imc <= 24.9)         System.out.println("Su IMC es de: " + imc + "Normal");
+
+        if (imc >= 25 && Imc <= 29.9)           System.out.println("Su IMC es de: " + imc + "Sobrepeso");
+
+        if (imc >= 30 && Imc <= 39.9)           System.out.println("Su IMC es de: " + imc + "Obesidad");
+        
+        else {
+                                                System.out.println("Su IMC es de: " + imc + "Obesidad morbida");
+
+        }
+    }
+}
