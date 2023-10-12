96e37f4cc7d3bc82ff74ba132c51d8bb04a45fff
LenScarlet666 <106494253+LenScarlet666@users.noreply.github.com>
2023-10-12 13:15:09 -0300

    Update Imc.java

--- Tp1/Imc.java
+++ Tp1/Imc.java
@@ -1,6 +1,6 @@
 public class Imc {
     public static void main(String[] args) {
-        System.out.println("Calclulo de IMC");
+        System.out.println("Calclulo de imc");
 
         System.out.print("Ingrese su altura en cm:");
         double altura = new java.util.Scanner(System.in).nextDouble();
@@ -10,19 +10,17 @@ public class Imc {
 
         if (imc <= 15)                          System.out.println("Su imc es de:" + imc + "Delgadez muy severa");
 
-        if (imc > 15 && Imc < 15.9)             System.out.println("Su IMC es de: " + imc + "Delgadez severa");
+        if (imc > 15 && imc < 15.9)             System.out.println("Su imc es de: " + imc + "Delgadez severa");
 
-        if (imc >= 16 && Imc < 18.4)            System.out.println("Su IMC es de: " + imc + "Delgadez");
+        if (imc >= 16 && imc < 18.4)            System.out.println("Su imc es de: " + imc + "Delgadez");
 
-        if (imc >= 18.5 && Imc <= 24.9)         System.out.println("Su IMC es de: " + imc + "Normal");
+        if (imc >= 18.5 && imc <= 24.9)         System.out.println("Su imc es de: " + imc + "Normal");
 
-        if (imc >= 25 && Imc <= 29.9)           System.out.println("Su IMC es de: " + imc + "Sobrepeso");
+        if (imc >= 25 && imc <= 29.9)           System.out.println("Su imc es de: " + imc + "Sobrepeso");
 
-        if (imc >= 30 && Imc <= 39.9)           System.out.println("Su IMC es de: " + imc + "Obesidad");
+        if (imc >= 30 && imc <= 39.9)           System.out.println("Su imc es de: " + imc + "Obesidad");
         
-        else {
-                                                System.out.println("Su IMC es de: " + imc + "Obesidad morbida");
-
-        }
+        if (imc >= 40)                          System.out.println("Su imc es de: " + imc + "Obesidad morbida");
+                                                
     }
 }
